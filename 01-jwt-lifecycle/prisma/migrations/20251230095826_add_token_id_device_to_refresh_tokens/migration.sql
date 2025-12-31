b07b80285942412c676e556dabac4b925b063acc
Quan Bui <quanbh1911@gmail.com>
2025-12-31 15:17:28 +0700

    Add temp

---
+++ 01-jwt-lifecycle/prisma/migrations/20251230095826_add_token_id_device_to_refresh_tokens/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[tokenId]` on the table `refresh_tokens` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `tokenId` to the `refresh_tokens` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "refresh_tokens" ADD COLUMN     "deviceInfo" TEXT,
+ADD COLUMN     "ipAddress" TEXT,
+ADD COLUMN     "lastUsedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "tokenId" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "refresh_tokens_tokenId_key" ON "refresh_tokens"("tokenId");
+
+-- CreateIndex
+CREATE INDEX "refresh_tokens_tokenId_idx" ON "refresh_tokens"("tokenId");
+
+-- CreateIndex
+CREATE INDEX "refresh_tokens_userId_tokenId_idx" ON "refresh_tokens"("userId", "tokenId");
